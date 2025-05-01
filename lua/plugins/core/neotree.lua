78f1c2719013bb1043b804ec76ee87e566b0a63a
Tsunami014 (Max) <96847801+Tsunami014@users.noreply.github.com>
2025-05-02 09:03:44 +1000

    Update neotree.lua

--- lua/plugins/core/neotree.lua
+++ lua/plugins/core/neotree.lua
@@ -1,5 +1,12 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  branch = "v3.x",
+  lazy = false, -- neo-tree will lazily load itself
   opts = {
     filesystem = {
       filtered_items = {
