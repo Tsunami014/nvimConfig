5040bb9f9e29d1dde6aa0f24b9d03471da258b73
Tsunami014 <tsunami014@duck.com>
2025-09-12 22:19:58 +1000

    Changed the default tab stuff

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -6,6 +6,10 @@ vim.g.maplocalleader = "\\"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.tabstop = 4    -- Sets the width of a tab character
+vim.opt.shiftwidth = 4 -- Sets the width used for auto-indentation and shifting commands
+vim.opt.expandtab = true -- Converts tabs to spaces
+
 require("lazy").setup({
   spec = {
     -- import your plugins
